6fa55bea825f6e3565f4b8322ffca3c4214f910f
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-05-01 15:43:38 -0300

    Add files via upload

---
+++ Ejercicio 1.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese su edad: ");
+
+        if (int.TryParse(Console.ReadLine(), out int edad))
+        {
+            bool esMayor = EsMayorDeEdad(edad);
+
+            if (esMayor)
+                Console.WriteLine("Es mayor de edad");
+            else
+                Console.WriteLine("Es menor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida");
+        }
+    }
+
+    public static bool EsMayorDeEdad(int edad)
+    {
+        return edad >= 18;
+    }
+}
---
+++ Ejercicio 3.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el radio del círculo: ");
+
+        if (double.TryParse(Console.ReadLine(), out double radio))
+        {
+            double area = CalcularAreaCirculo(radio);
+            ImprimirResultado("EL ÁREA DEL CÍRCULO ES", area);
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida");
+        }
+    }
+
+    public static double CalcularAreaCirculo(double radio)
+    {
+        return Math.PI * Math.Pow(radio, 2);
+    }
+
+    public static void ImprimirResultado(string texto, double valor)
+    {
+        Console.WriteLine($"--- {texto}: {Math.Round(valor, 2)} ---");
+    }
+}
