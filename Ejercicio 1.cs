39e6194f8e2bc47a66a2c712f031a5cf536aab64
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-05-31 18:11:22 -0300

    Add files via upload

---
+++ Ejercicio 1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static int ProductoEscalar(int[] array1, int[] array2)
+    {
+        int suma = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            suma += array1.Length * array2[i];
+        }
+        return suma;
+    }
+
+    static int[] InvertirArray(int[] array)
+    {
+        int[] invertido = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            invertido[i] = array[array.Length - 1 - i];
+        }
+        return invertido;
+    }
+
+    static int ProductoEscalarConInverso(int[] array1, int[] array2)
+    {
+        int[] arrayInvertido = InvertirArray(array2);
+        return ProductoEscalar(array1, arrayInvertido);
+    }
+
+    static void Main()
+    {
+        int[] a = { 1, 2, 3 };
+        int[] b = { 4, 5, 6 };
+        int resultado = ProductoEscalarConInverso(a, b);
+        Console.WriteLine("Producto escalar con inverso: " + resultado);
+    }
+}
---
+++ Ejercicio 2.cs
@@ -0,0 +1,41 @@
+﻿/*Ejercicio 2:
+Escribir una función/método llamado busquedaL que toma un arreglo de enteros y un
+entero. Si el entero se encuentra entre los elementos del arreglo, el método debe devolver
+la posición en la que se encuentra, caso contrario debe devolver un valor que permita
+identificar a quien llama a la función/método que el entero no se encuentra en el arreglo.
+*/
+
+using System;
+class Program
+{
+    static int busquedaL(int[] array, int num)
+    {
+        for (int i = 0; i < array.Length; i++) // Para dar valor a la posicion.
+        {
+            if (array[i] == num) // Esto compara cada elemento del arreglo con el numero buscado.
+            {
+                return i; // Aca devuelve la posición donde encontro el numero. 
+            }
+        }
+        return -1; /* Esto si no se encuentra el arreglo con el
+                      numero buscado y terminaria en el else*/
+    }
+    static void Main()
+    {
+        int[] num = { 5, 8, 12, 20, 30 };
+        Console.Write("Ingrese un numero para buscar: ");
+        int numbuscado;
+        int.TryParse(Console.ReadLine(), out numbuscado);
+        int total = busquedaL(num, numbuscado); /* Empieza a ejecutar el metodo para buscar el numero
+                                                   y guardar la respuesta en total */
+
+        if (total != -1) 
+        {
+            Console.WriteLine("El numero esta en la posicion " + total);
+        }
+        else
+        {
+            Console.WriteLine("El numero no se encuentra en el arreglo");
+        }
+    }
+}
