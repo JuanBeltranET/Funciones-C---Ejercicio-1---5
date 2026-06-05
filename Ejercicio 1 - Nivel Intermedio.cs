ffcf2566b3a1943b6e9684621e7c44bd263e1885
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-06-05 10:19:40 -0300

    Add files via upload

---
+++ Ejercicio 1 - Nivel Intermedio.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void InvertirArray(string[] nombres)
+    {
+        int inicio = 0;
+        int fin = nombres.Length - 1;
+        while (inicio < fin)
+        {
+            string iniciotemp = nombres[inicio]; // guardo temporalmente un valor para poder intercambiar
+            nombres[inicio] = nombres[fin];
+            nombres[fin] = iniciotemp;
+            inicio++;
+            fin--; // sirven para mover los índices hacia el centro del arreglo despues de cada intercambio
+        }
+    }
+    static void Main()
+    {
+        string[] nombres = { "Juan", "Demian", "Alejandro", "Jose", "Anabella", "Gabriel", "Ivo", "Isaias", "Leonardo", "Matias", "Lautaro", "Nico", "Franco", "Tiziano" };
+        InvertirArray(nombres);
+        foreach (string nombre in nombres)
+        {
+            Console.WriteLine(nombre);
+        }
+    }
+}
