c1ea4e78a7766a574adf5a6a1a2209caf2f2376f
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-06-05 08:26:42 -0300

    Add files via upload

---
+++ Ejercicio 1 - Nivel Basico.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] num = { 5, 8, 12, 3, 7, 10, 15, 6, 9, 4 };
+        int suma = 0;
+        for (int i = 0; i < num.Length; i++)
+        {
+            suma = suma + num[i];
+        }
+        double promedio = (double)suma / num.Length;
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Promedio: " + promedio);
+    }
+}
