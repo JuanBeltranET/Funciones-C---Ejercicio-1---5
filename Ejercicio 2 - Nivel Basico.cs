bda0ddce4f709a08fdbbb89b9e38edb420d0c2cb
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-06-05 08:59:17 -0300

    Add files via upload

---
+++ Ejercicio 2 - Nivel Basico.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] num = { 25, 8, 42, 17, 3, 30, 15, 10 };
+        int mayor = num[0];
+        int menor = num[0];
+        int posMayor = 0;
+        int posMenor = 0;
+        for (int i = 1; i < num.Length; i++)
+        {
+            if (num[i] > mayor)
+            {
+                mayor = num[1];
+                posMayor = i;
+            }
+            if (num[i] < menor)
+            {
+                menor = num[i];
+                posMenor = i;
+            }
+        }
+        Console.WriteLine("Mayor: " + mayor);
+        Console.WriteLine("Posicion del mayor: " + posMayor);
+        Console.WriteLine("Menor: " + menor);
+        Console.WriteLine("Posicion del mayor: " + posMenor);
+    }
+}
