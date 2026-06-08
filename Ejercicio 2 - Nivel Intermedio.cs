1db09194604c84c012f1d491cbf4d42ed4495bdd
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-06-08 11:44:15 -0300

    Add files via upload

---
+++ Ejercicio 2 - Nivel Intermedio.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 12, 7, 19, 24, 31, 8, 15, 41, 3, 10, 22, 29, 6, 17, 4 };
+        int cantidadPrimos = 0;
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if (EsPrimo(numeros[i]))
+            {
+                cantidadPrimos++;
+            }
+        }
+        int[] primos = new int[cantidadPrimos];
+        int j = 0;
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if (EsPrimo(numeros[i]))
+            {
+                primos[j] = numeros[i];
+                j++;
+            }
+        }
+        Console.WriteLine("Numeros primos:");
+        for (int i = 0; i < primos.Length; i++)
+        {
+            Console.Write(primos[i] + " ");
+        }
+    }
+
+    static bool EsPrimo(int numero)
+    {
+        if (numero < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i < numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
