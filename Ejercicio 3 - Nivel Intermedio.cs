b53e96f0394d6cb9622281cee529d49946194c30
JuanBeltranET <etjuanmanuelbeltran@gmail.com>
2026-06-12 08:03:16 -0300

    Add files via upload

---
+++ Ejercicio 3 - Nivel Intermedio.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        char[] letras = { 'a', 'b', 'a', 'c', 'b', 'a', 'd', 'c', 'a', 'b' };
+
+        bool[] contado = new bool[letras.Length];
+
+        for (int i = 0; i < letras.Length; i++)
+        {
+            if (!contado[i])
+            {
+                int cantidad = 1;
+
+                for (int j = i + 1; j < letras.Length; j++)
+                {
+                    if (letras[i] == letras[j])
+                    {
+                        cantidad++;
+                        contado[j] = true;
+                    }
+                }
+
+                Console.WriteLine("'" + letras[i] + "' aparece " + cantidad + " veces");
+            }
+        }
+    }
+}
